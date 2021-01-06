d402b05379760ec0322ed045f2070696936f3c9e
Andrey <andreyba2000@gmail.com>
2021-01-06 12:06:50 +0200

    Added str and equals

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -6,8 +6,8 @@ class DiGraph(GraphInterface):
         self.__Nodes = {}
         self.__Edges = {}
         self.__Backward_Edges = {}
-        self.MC = 0
-        self.edge_size = 0
+        self.__MC = 0
+        self.__edge_size = 0
 
     def copy(self):
         pass
@@ -16,10 +16,10 @@ class DiGraph(GraphInterface):
         return len(self.__Nodes)
 
     def e_size(self) -> int:
-        return self.edge_size
+        return self.__edge_size
 
     def get_mc(self) -> int:
-        return self.MC
+        return self.__MC
 
     def add_edge(self, id1: int, id2: int, weight: float) -> bool:
         if not (id1 in self.__Nodes.keys() and id2 in self.__Nodes.keys()):
@@ -29,12 +29,12 @@ class DiGraph(GraphInterface):
                 return False
             self.__Edges[id1][id2] = weight
             self.__Backward_Edges[id2][id1] = weight
-            self.MC += 1
+            self.__MC += 1
             return True
         self.__Edges[id1][id2] = weight
         self.__Backward_Edges[id2][id1] = weight
-        self.MC += 1
-        self.edge_size += 1
+        self.__MC += 1
+        self.__edge_size += 1
         return True
 
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
@@ -43,7 +43,7 @@ class DiGraph(GraphInterface):
         self.__Nodes[node_id] = node_data(node_id)
         self.__Edges[node_id] = {}
         self.__Backward_Edges[node_id] = {}
-        self.MC += 1
+        self.__MC += 1
         return True
 
     def remove_node(self, node_id: int) -> bool:
@@ -51,12 +51,12 @@ class DiGraph(GraphInterface):
             return False
         for key in self.__Backward_Edges[node_id].keys():
             self.__Edges[key].pop(node_id)
-            self.edge_size -= 1
-            self.MC += 1
+            self.__edge_size -= 1
+            self.__MC += 1
         self.__Edges.pop(node_id)
         self.__Backward_Edges.pop(node_id)
         self.__Nodes.pop(node_id)
-        self.MC += 1
+        self.__MC += 1
         return True
 
     def remove_edge(self, node_id1: int, node_id2: int) -> bool:
@@ -65,8 +65,8 @@ class DiGraph(GraphInterface):
         if node_id2 in self.__Edges[node_id1].keys():
             self.__Edges[node_id1].pop(node_id2)
             self.__Backward_Edges[node_id2].pop(node_id1)
-            self.edge_size -= 1
-            self.MC += 1
+            self.__edge_size -= 1
+            self.__MC += 1
 
     def get_all_v(self) -> dict:
         return self.__Nodes
@@ -77,4 +77,14 @@ class DiGraph(GraphInterface):
     def all_out_edges_of_node(self, id1: int) -> dict:
         return self.__Edges[id1]
 
+    def __str__(self):
+        return "DiGraph: { " + "Node size: " + str(len(self.__Nodes)) + ", Edge size: " + str(self.__edge_size) + ", MC: " + str(self.__MC) + " }"
 
+    def __eq__(self, other):
+        if isinstance(other, GraphInterface):
+            if other.get_all_v() == self.__Nodes and other.__edge_size == self.__edge_size:
+                for edge in other.get_all_v().keys():
+                    if edge in self.__Nodes and other.all_out_edges_of_node(edge) != self.__Edges[edge]:
+                        return False
+                return True
+        return False
--- src/node_data.py
+++ src/node_data.py
@@ -3,9 +3,33 @@ class node_data:
 
     def __init__(self, key):
         self.__key = key
-        self.tag = tag = 0
-        self.weight = 0
-        self.pos = (0, 0, 0)
+        self.__tag = tag = 0
+        self.__weight = 0
+        self.__pos = (0, 0, 0)
 
-    def GetKey(self):
+    def getkey(self) -> int:
         return self.__key
+
+    def gettag(self):
+        return self.__tag
+
+    def getweight(self):
+        return self.__weight
+
+    def getpos(self):
+        return self.__pos
+
+    def settag(self, tag):
+        self.__tag = tag
+
+    def setweight(self, weight):
+        self.__weight = weight
+
+    def __repr__(self):
+        return "node_data: { Key: " + str(self.__key) + " }"
+
+    def __eq__(self, other):
+        if isinstance(other, node_data):
+            if other.getkey() == self.__key:
+                return True
+        return False
