06d5484375121495f3a7d2d3b46ce9f520d0e879
Andrey <andreyba2000@gmail.com>
2021-01-08 20:41:07 +0200

    Fixed tests

--- Tests/test_DiGraph.py
+++ Tests/test_DiGraph.py
@@ -6,10 +6,10 @@ import random
 
 class TestDiGraph(TestCase):
 
-    # def test_runtime(self):
-    #     v = 100000
-    #     e = v * 10
-        # graph = graph_creator(v, e)
+    def test_runtime(self):
+        v = 100000
+        e = v * 10
+        graph = graph_creator(v, e)
 
     def test_copy(self):
         v = 10000
@@ -31,14 +31,14 @@ class TestDiGraph(TestCase):
         graph = DiGraph()
         for i in range(6):
             graph.add_node(i)
-        graph.add_edge(0, 1, 5)
-        graph.add_edge(1, 1, 3)
+        graph.add_edge(0, 1, 5)  # 1
+        graph.add_edge(1, 1, 3)  # 2
         TestCase.assertEqual(self, 5, graph.get_edge(0, 1))
         TestCase.assertEqual(self, -1, graph.get_edge(1, 0))
         TestCase.assertEqual(self, 2, graph.e_size())
-        graph.add_edge(0, 2, 3)
+        graph.add_edge(0, 2, 3)  #3
         TestCase.assertEqual(self, 3, graph.e_size())
-        graph.add_edge(0, 3, 3)
+        graph.add_edge(0, 3, 3)  #4
         TestCase.assertEqual(self, 3, graph.get_edge(0, 3))
         TestCase.assertEqual(self, 4, graph.e_size())
         graph.add_edge(0, 3, 3)
@@ -46,8 +46,8 @@ class TestDiGraph(TestCase):
         graph.add_edge(0, 10, 3)
         TestCase.assertEqual(self, 4, graph.e_size())
         graph.add_edge(0, 3, 2)
-        TestCase.assertEqual(self, 3, graph.get_edge(0, 3))
-        TestCase.assertEqual(self, 3, graph.e_size())
+        TestCase.assertEqual(self, 2, graph.get_edge(0, 3))
+        TestCase.assertEqual(self, 4, graph.e_size())
 
     def test_remove_edge(self):
         graph = DiGraph()
