d23302dd19e5d2bff8c4965b321b91da37d76ac0
Mike <Miket9717@gmail.com>
2021-01-12 14:38:24 +0200

    made comparison test

---
+++ src/NotUnitTests/PlotCompare_mike2.py
@@ -0,0 +1,119 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_data(java_stats, python_stats, networkx_stats, labels):
+    width = 0.35  # the width of the bars
+
+    ind = np.arange(len(java_stats))  # the x locations for the groups
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(ind - width / 2, java_stats, width,
+                    label='java')
+    rects2 = ax.bar(ind, python_stats, width,
+                    label='python')
+    if networkx_stats is not None:
+        rects3 = ax.bar(ind + width / 2, networkx_stats, width * 0.8,
+                        label='networkx')
+        autolabel(rects3, ax, "right")
+
+    autolabel(rects1, ax, "left")
+    autolabel(rects2, ax, "center")
+
+    ax.set_xticks(ind)
+    ax.set_xticklabels(labels)
+    ax.legend()
+
+    return fig, ax
+
+
+def autolabel(rects, ax, xpos='center'):
+    """
+    Attach a text label above each bar in *rects*, displaying its height.
+
+    *xpos* indicates which side to place the text w.r.t. the center of
+    the bar. It can be one of the following {'center', 'right', 'left'}.
+    """
+
+    ha = {'center': 'center', 'right': 'left', 'left': 'right'}
+    offset = {'center': 0, 'right': 1, 'left': -1}
+
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(offset[xpos] * 3, 3),  # use 3 points offset
+                    textcoords="offset points",  # in both directions
+                    ha=ha[xpos], va='bottom')
+
+
+def plot_jsons():
+    java_times = [20, 35, 30, 35, 27, 8]
+    python_times = [25, 32, 34, 20, 25, 9]
+    network_times = [22, 33, 29, 19, 26, 10]
+    x_labels = ['G_10_80', 'G_100_800', 'G_1k_80k', 'G_10k_80k',
+                'G_20k_160k', 'G_30k_240k']
+
+    figp, axp = plot_data(java_times, python_times, network_times, x_labels)
+    axp.set_ylabel('time to calculate')
+    axp.set_title('shortest path')
+
+    figp.tight_layout()
+
+    plt.show()
+
+
+def plot_scc(file):
+    java_times = [0.437, 0.760, 2.905, 1.24, 4.98, 0]
+    python_times = [4.09, 2.33, 9.74, 5.32, 18.56, 27.76]
+    network_times = [0.50, 1.51, 6.00, 3.50, 5.359, 18.16]
+    x_labels = ['G_100k_100k', 'G_100k_1m', 'G_100k_5m', 'G_1m_0',
+                'G_1m_1m', 'G_1m_10m']
+
+    figp, axp = plot_data(java_times, python_times, network_times, x_labels)
+    axp.set_ylabel('time to calculate')
+    axp.set_title('strongly connected components')
+
+    figp.tight_layout()
+
+    plt.savefig("../../data_local/" + file, dpi=150)
+    plt.show()
+
+
+def plot_scc_single(file):
+    java_times = [0.06, 0.64, 1.57]
+    python_times = [0.12, 2.32, 9.64]
+    network_times = None
+    x_labels = ['G_100k_100k', 'G_100k_1m', 'G_100k_5m']
+
+    figp, axp = plot_data(java_times, python_times, network_times, x_labels)
+    axp.set_ylabel('time to calculate')
+    axp.set_title('strongly connected component')
+
+    figp.tight_layout()
+
+    plt.savefig("../../data_local/" + file, dpi=150)
+    plt.show()
+
+
+def plot_paths(file):
+    java_times = [0.01, 0.16, 0.58]
+    python_times = [0.00, 1.88, 4.99]
+    network_times = [0.00, 0.44, 2.31]
+    x_labels = ['G_100k_100k', 'G_100k_1m', 'G_100k_5m']
+
+    figp, axp = plot_data(java_times, python_times, network_times, x_labels)
+    axp.set_ylabel('time to calculate')
+    axp.set_title('shortest path')
+
+    figp.tight_layout()
+
+    plt.savefig("../../data_local/" + file, dpi=150)
+    plt.show()
+
+
+if __name__ == '__main__':
+    # plot_scc()
+    plot_paths("paths.jpg")
+    plot_scc_single("scc_single.jpg")
+    plot_scc("scc.jpg")
---
+++ src/NotUnitTests/RuntimeCompare.py
@@ -0,0 +1,124 @@
+from src.DiGraph import DiGraph
+from src.GraphAlgo import GraphAlgo
+import time
+import networkx as nx
+from src.utilities.Jsonconverter import load_networkx_graph
+
+graph: DiGraph = None
+algo: GraphAlgo = None
+v = 100000
+e = v * 10
+
+path = "../../MyGraph"
+save = e < 5000001
+
+tries = 25
+
+
+def test_load(file):
+    global algo
+    algo = GraphAlgo()
+    algo.load_from_json(file)
+
+
+def do_test(file):
+    t = time.time()
+    print("\n-----------", file, "--------------")
+    print("- python -")
+    test_load(file)
+    print(algo.get_graph())
+
+    print("python load time:", time.time() - t)
+
+    print("\n- nx -")
+    t = time.time()
+    nx_graph = load_networkx_graph(file)
+    print("NetworkX load:", time.time() - t)
+
+    print("- - - - - - - - - - -")
+    print("compute avg path time:")
+    print("- python -")
+    t = time.time()
+
+    # print(algo.shortest_path(0, 1000))
+
+    fail = 0
+    avg_dist = 0
+    for i in range(tries):
+        dist = algo.shortest_path(0, i + 1)
+        if dist[0] == float("inf"):
+            fail += 1
+        else:
+            avg_dist += dist[0]
+
+    end_time = time.time() - t
+    print("python avg dist:", avg_dist / tries)
+    print("python run:", tries, ",number of times path not found:", fail)
+    print("python avg path time:", end_time / tries)
+
+    print("\n- nx -")
+
+    t = time.time()
+    # components = nx.connected_components(nx_graph)
+    # nx.connected_components(nx_graph)
+
+    fail = 0
+    avg_dist = 0
+    for i in range(tries):
+        old = time.time()
+        try:
+            avg_dist += nx.shortest_path_length(nx_graph, 0, i + 1, 'weight')
+
+            # print("netx path dist : ", nx.shortest_path_length(nx_graph, 0, i, 'weight'))
+        except:
+            fail += 1
+            # print("no path")
+            pass
+        # print(time.time() - old)
+    end_time = time.time() - t
+    print("NetworkX avg dist:", avg_dist / tries)
+    print("NetworkX run:", tries, ",number of times path not found:", fail)
+    print("NetworkX avg path time:", end_time / tries)
+
+    print("- - - - - - - - - - -")
+    print("compute single component time:")
+    print("- python -")
+    g = GraphAlgo(algo.get_graph())
+    t = time.time()
+
+    avg_len = 0
+    for i in range(tries):
+        avg_len += len(g.connected_component(i))
+    end_time = time.time() - t
+    print("python avg single component length:", avg_len / tries)
+    # print(g.connected_component(0))
+    print("python avg single component time:", end_time / tries)
+
+    print("- - - - - - - - - - -")
+    print("compute components time:")
+    print("- python -")
+
+    t = time.time()
+    c = len(g.connected_components())
+    print("python number of components:", c)
+    print("python components:", time.time() - t)
+
+    print("\n- nx -")
+
+    t = time.time()
+    # components = nx.connected_components(nx_graph)
+    # nx.connected_components(nx_graph)
+    c = sorted(nx.strongly_connected_components(nx_graph), key=len, reverse=True)
+    print("NetworkX number of components:", len(c))
+    print("NetworkX components:", time.time() - t)
+
+
+if __name__ == '__main__':
+    directory = "../../data_local/"
+    # do_test(directory + "g_100k_5m")
+    # arr = ["g_100k_100k", "g_100k_1m", "g_100k_5m", "g_1m_0", "g_1m_1m", "g_1m_10m"]
+    directory = "../../data/"
+    arr = ["G_10_80_0.json", "G_100_800_0.json", "G_1000_8000_0.json", "G_10000_80000_0.json",
+           "G_20000_160000_0.json", "G_30000_240000_0.json"]
+    for name in arr:
+        do_test(directory + name)
